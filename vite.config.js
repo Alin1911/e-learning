c60e64f21145741aabe2d0645188c894e197c0e6
Alin <andreialin2324@yahoo.com>
2023-03-05 23:20:57 +0200

    add sass

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
+import sass from 'sass';
 
 export default defineConfig({
     plugins: [
@@ -8,5 +9,13 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+	vue(),
     ],
+    css: {
+    	preprocessorOptions: {
+      		sass: {
+        		implementation: sass,
+      		},
+    	},
+    },
 });
