9d5ae46aa491c07271a1ed421999a9cf02547eeb
Alin <andreialin2324@yahoo.com>
2023-03-17 10:10:06 +0200

    start

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
-import sass from 'sass';
+
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-	vue(),
+        vue(),
+        laravel([
+          'resources/sass/app.scss',
+          'resources/css/app.css',
+          'resources/js/app.js',
+        ]),
     ],
-    css: {
-    	preprocessorOptions: {
-      		sass: {
-        		implementation: sass,
-      		},
-    	},
-    },
 });
+
