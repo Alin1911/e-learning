79ec0bb708b9b48d4517be485d0457c869e520c0
Alin <andreialin2324@yahoo.com>
2023-05-05 21:17:27 +0300

    add adctive

--- resources/views/course/create.blade.php
+++ resources/views/course/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div>
-   @include('widgets.slider_course_add')
+   @include('widgets.slider_course_add', ['active' => 1])
 <div class="main-content container">
     <course-add :categories={{$categories}}></course-add>
 </div>
--- resources/views/widgets/slider_course_add.blade.php
+++ resources/views/widgets/slider_course_add.blade.php
@@ -2,20 +2,20 @@
     <div class="position-sticky">
         <div class="list-group list-group-flush">
           <a
-            href="#"
-            class="list-group-item list-group-item-action py-3 px-5 ripple active"
+            href="/course/create"
+            class="list-group-item list-group-item-action py-3 px-5 ripple @if($active == 1) active @endif"
             aria-current="true"
           >
             <i class="fas fa-solid fa-book fa-fw me-3"></i><span>Adaugare curs</span>
           </a>
-          <a href="/course/lesson/create" class="list-group-item list-group-item-action px-5 py-3 ripple">
+          <a href="/course/lesson/create" class="list-group-item list-group-item-action px-5 py-3 ripple @if($active == 2) active @endif">
             <i class="fas fa-solid fa-person-chalkboard fa-fw me-3 "></i><span>Adauga lectie</span>
           </a>
-          <a href="#" class="list-group-item list-group-item-action py-3 px-5 ripple"
+          <a href="#" class="list-group-item list-group-item-action py-3 px-5 ripple @if($active == 3) active @endif"
             ><i class="fas fa-duotone fa-check-double fa-fw me-3"></i><span>Adauga Test</span></a
           >
 
-          <a href="#" class="list-group-item list-group-item-action px-5 py-3 ripple"
+          <a href="#" class="list-group-item list-group-item-action px-5 py-3 ripple @if($active == 4) active @endif"
             ><i class="fas fa-users fa-fw me-3"></i><span>Creaza o discoutie</span></a
           >
         </div>
