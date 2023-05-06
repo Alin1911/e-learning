2e9d0a7f97e1e86eb499f503855a24e4aeaffa3e
Alin <andreialin2324@yahoo.com>
2023-05-07 00:14:19 +0300

    add exercices logic

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ExerciseOption;
+use App\Models\ExerciseOrderingItem;
+use App\Models\ExerciseFillInTheBlankItem;
+
+
 use App\Models\Exercise;
 use Illuminate\Http\Request;
 
@@ -21,9 +26,52 @@ class ExerciseController extends Controller
     public function store(Request $request)
     {
         $exercise = new Exercise();
-        $exercise->title = $request->title;
-        $exercise->description = $request->description;
+        if ($request->has('test_id')) {
+            $exercise->test_id = $request->test_id;
+        }
+        if ($request->has('question')) {
+            $exercise->question = $request->question;
+        }
+        if ($request->has('exercise_type')) {
+            $exercise->exercise_type = $request->exercise_type;
+        }
+        if ($request->has('points')) {
+            $exercise->points = $request->points;
+        }
         $exercise->save();
+
+        if ($request->has('options')) {
+            $options = json_decode($request->options, true);
+            foreach ($options as $option) {
+                $exerciseOption = new ExerciseOption();
+                $exerciseOption->exercise_id = $exercise->id;
+                $exerciseOption->answer = $option['answer'];
+                $exerciseOption->correct = $option['correct'];
+                $exerciseOption->save();
+            }
+        }
+
+        if ($request->has('ordering_items')) {
+            $orderingItems = json_decode($request->ordering_items, true);
+            foreach ($orderingItems as $item) {
+                $orderingItem = new ExerciseOrderingItem();
+                $orderingItem->exercise_id = $exercise->id;
+                $orderingItem->item = $item['item'];
+                $orderingItem->correct_order = $item['correct_order'];
+                $orderingItem->save();
+            }
+        }
+
+        if ($request->has('fill_in_the_blank_items')) {
+            $fillInTheBlankItems = json_decode($request->fill_in_the_blank_items, true);
+            foreach ($fillInTheBlankItems as $item) {
+                $fillInTheBlankItem = new ExerciseFillInTheBlankItem();
+                $fillInTheBlankItem->exercise_id = $exercise->id;
+                $fillInTheBlankItem->word = $item['word'];
+                $fillInTheBlankItem->position = $item['position'];
+                $fillInTheBlankItem->save();
+            }
+        }
 
         return redirect()->route('exercise.index');
     }
@@ -51,4 +99,40 @@ class ExerciseController extends Controller
         $exercise = Exercise::find($id);
         $exercise->delete();
     }
+
+    public function checkAnswer(Request $request, $exercise_id)
+    {
+        $exercise = Exercise::find($exercise_id);
+
+        if (! $exercise) {
+            return response()->json(['message' => 'Exercise not found'], 404);
+        }
+
+        $isCorrect = false;
+
+        switch ($exercise->exercise_type) {
+            case 'multiple_choice_multiple_answers':
+                $isCorrect = $exercise->checkMultipleChoiceMultipleAnswers($request->answers);
+                break;
+
+            case 'multiple_choice_single_answer':
+                $isCorrect = $exercise->checkMultipleChoiceSingleAnswer($request->answer);
+                break;
+
+            case 'numeric':
+                $isCorrect = $exercise->checkNumericAnswer($request->answer);
+                break;
+
+            case 'ordering':
+                $isCorrect = $exercise->checkOrdering($request->ordering);
+                break;
+
+            case 'fill_in_the_blank':
+                $isCorrect = $exercise->checkFillInTheBlank($request->words);
+                break;
+        }
+
+        return response()->json(['isCorrect' => $isCorrect]);
+    }
+
 }
--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -14,4 +14,50 @@ class Exercise extends Model
         return $this->belongsTo(Course::class);
     }
 
+    public function options()
+    {
+        return $this->hasMany(ExerciseOption::class);
+    }
+
+    public function orderingItems()
+    {
+        return $this->hasMany(ExerciseOrderingItem::class);
+    }
+
+    public function fillInTheBlankItems()
+    {
+        return $this->hasMany(ExerciseFillInTheBlankItem::class);
+    }
+
+    public function checkMultipleChoiceMultipleAnswers($answers)
+    {
+        $correctOptions = $this->options()->where('correct', true)->pluck('id')->toArray();
+        sort($correctOptions);
+        sort($answers);
+        return $correctOptions === $answers;
+    }
+
+    public function checkMultipleChoiceSingleAnswer($answer)
+    {
+        $correctOption = $this->options()->where('correct', true)->first();
+        return $correctOption->id === $answer;
+    }
+
+    public function checkNumericAnswer($answer)
+    {
+        return $this->numeric_answer == $answer;
+    }
+
+    public function checkOrdering($ordering)
+    {
+        $correctOrdering = $this->orderingItems()->orderBy('correct_order')->pluck('id')->toArray();
+        return $correctOrdering === $ordering;
+    }
+
+    public function checkFillInTheBlank($words)
+    {
+        $correctWords = $this->fillInTheBlankItems()->orderBy('position')->pluck('word')->toArray();
+        return $correctWords === $words;
+    }
+
 }
