6855b7e416ec75b5751316b5736aaa653fa9c72f
Alin <andreialin2324@yahoo.com>
2023-05-14 21:10:08 +0300

    fix ecercise view

--- resources/views/test/edit.blade.php
+++ resources/views/test/edit.blade.php
@@ -5,10 +5,19 @@
     <h1>Test#{{$test->id}}  {{$test->title}}</h1>
     <h2>Course: {{$test->course->title}}</h2>
     <div class="main-content container">
+        @if(!empty($test->exercises))
+        <hr>
+        <h4> Exerciții: </h4>
         @foreach ($test->exercises as $e)
-            {{$e->id}} <br>    
+        <div class="d-flex justify-content-between p-2  border-bottom">
+            <h5>{{$e->id}}. Întrebare: {{$e->question}}</h5>
+            <button class="btn btn-danger rounded-pill" href="/exercise/{{$e->id}}/delete">Șterge exercițiu</button>
+        </div>
         @endforeach
+        @endif
+        <div class="mt-3">
         <exercise-create :test_id="{{$test->id}}"></exercise-create>
+        </div>
     </div>
 </div>
 @endsection
