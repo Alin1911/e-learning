f3ee3dfd51da25d38f13e3af9c3ab52822a36bd6
Alin <andreialin2324@yahoo.com>
2023-05-16 11:50:16 +0300

    add file

---
+++ .php-cs-fixer.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = Symfony\Component\Finder\Finder::create()
+    ->notPath('vendor')
+    ->notPath('bootstrap')
+    ->exclude('storage') // Schimbă această linie
+    ->in(__DIR__)
+    ->name('*.php')
+    ->notName('*.blade.php');
+
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRules([
+        '@PSR2' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
