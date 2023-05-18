f028f3e4ae9a4e6d5fb56e72df2f5f3a6914b2f1
Alin <andreialin2324@yahoo.com>
2023-05-18 22:34:40 +0300

    add controller rout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,24 @@ class UserController extends Controller
 		}
 		return json_encode(auth()->user());
 	}
+
+	public function edit($id)
+	{
+		$user = auth()->user();
+		return view('account.edit')->with(['user' => $user]);
+	}
+
+	public function update(Request $request, $id)
+	{
+		$user = auth()->user();
+		$user->update($request->all());
+		return redirect()->route('account.edit', $user->id);
+	}
+
+	public function destroy($id)
+	{
+		$user = auth()->user();
+		$user->delete();
+		return redirect()->route('home');
+	}
 }
