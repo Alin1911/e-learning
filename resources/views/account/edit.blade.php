ca4c42d4d2aba7044ec1ba2711b602af47686193
Alin <andreialin2324@yahoo.com>
2023-05-18 22:36:39 +0300

    add simple blade

---
+++ resources/views/account/edit.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.app")
+
+@section("content")
+	<div class="container my-5 bg-light pb-5 pt-3 rounded shadow">
+	</div>
+@endsection
---
+++ resources/views/account/index.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.app")
+
+@section("content")
+	<div class="container bg-white p-5 mt-4 shadow">
+	</div>
+@endsection
