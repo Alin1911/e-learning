488c132dc850eab2cc6924552f6ae7692194afe2
Alin <andreialin2324@yahoo.com>
2023-05-28 11:07:59 +0300

    fix migration

--- database/migrations/2023_05_28_080449_add_time_to_tests_table.php
+++ database/migrations/2023_05_28_080449_add_time_to_tests_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tests', function (Blueprint $table) {
-            $table->time('time')->after('description');
+            $table->integer('time')->after('description');
         });
     }
 
