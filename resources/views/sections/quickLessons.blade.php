fc7d926ebd400ce4b284844aedc572514a768b3d
Alin <andreialin2324@yahoo.com>
2023-06-16 20:39:25 +0300

    fix des

--- resources/views/sections/quickLessons.blade.php
+++ resources/views/sections/quickLessons.blade.php
@@ -1,13 +1,14 @@
 <div class="col-md-4">
-    <div class="card">
-        <div class="card-body" style="min-height: 260px">
-            <h2 class="card-title">Lecții Rapide</h2>
-            <p class="card-text">
-                Învăță ce ai nevoie în doar câteva minute cu lecțiile noastre de micro-learning. Eficiente, concise și ușor de asimilat.
-            </p>
-            <a href="/course/quick-lessons" class="btn btn-danger">
-                Încearcă o Lecție Rapidă
-            </a>
-        </div>
-    </div>
+	<div class="card">
+		<div class="card-body" style="min-height: 260px">
+			<h2 class="card-title">Lecții Rapide</h2>
+			<p class="card-text">
+				Învăță ce ai nevoie în doar câteva minute cu lecțiile noastre de
+				micro-learning. Eficiente, concise și ușor de asimilat.
+			</p>
+			<a href="/course/quick-lessons" class="btn btn-danger">
+				Încearcă o Lecție Rapidă
+			</a>
+		</div>
+	</div>
 </div>
