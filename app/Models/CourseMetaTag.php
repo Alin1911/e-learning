d0bd7570b64d0c7affa12d4b3c7d893f990180c4
Alin <andreialin2324@yahoo.com>
2023-06-16 23:27:47 +0300

    set search in lessons

--- app/Models/CourseMetaTag.php
+++ app/Models/CourseMetaTag.php
@@ -32,27 +32,33 @@ class CourseMetaTag extends Model
 	public static function countSegmentOccurrences($searchString, $segmentLength)
 	{
 		$segments = self::splitStringIntoSegments($searchString, $segmentLength);
-
-		$query = DB::table('course_meta_tags');
-
+	
+		$query = DB::table('course_meta_tags')
+				   ->join('lessons', 'course_meta_tags.course_id', '=', 'lessons.course_id');
+	
 		$totalCounts = [];
 		foreach ($segments as $index => $segment) {
 			$countColumn = '(
-                (LENGTH(title) - LENGTH(REPLACE(title, "'.$segment.'", "")))/'.$segmentLength.' +
-                (LENGTH(description) - LENGTH(REPLACE(description, "'.$segment.'", "")))/'.$segmentLength.' +
-                (LENGTH(keywords) - LENGTH(REPLACE(keywords, "'.$segment.'", "")))/'.$segmentLength.' +
-                (LENGTH(language) - LENGTH(REPLACE(language, "'.$segment.'", "")))/'.$segmentLength.' +
-                (LENGTH(author) - LENGTH(REPLACE(author, "'.$segment.'", "")))/'.$segmentLength.'
-            )';
+				(LENGTH(course_meta_tags.title) - LENGTH(REPLACE(course_meta_tags.title, "'.$segment.'", "")))/'.$segmentLength.' +
+				(LENGTH(course_meta_tags.description) - LENGTH(REPLACE(course_meta_tags.description, "'.$segment.'", "")))/'.$segmentLength.' +
+				(LENGTH(course_meta_tags.keywords) - LENGTH(REPLACE(course_meta_tags.keywords, "'.$segment.'", "")))/'.$segmentLength.' +
+				(LENGTH(course_meta_tags.language) - LENGTH(REPLACE(course_meta_tags.language, "'.$segment.'", "")))/'.$segmentLength.' +
+				(LENGTH(course_meta_tags.author) - LENGTH(REPLACE(course_meta_tags.author, "'.$segment.'", "")))/'.$segmentLength.' +
+				(LENGTH(lessons.title) - LENGTH(REPLACE(lessons.title, "'.$segment.'", "")))/'.$segmentLength.' +
+				(LENGTH(lessons.description) - LENGTH(REPLACE(lessons.description, "'.$segment.'", "")))/'.$segmentLength.' +
+				(LENGTH(lessons.content) - LENGTH(REPLACE(lessons.content, "'.$segment.'", "")))/'.$segmentLength.'
+			)';
 			$query->addSelect(DB::raw($countColumn.' as count'.$index));
 			$totalCounts[] = $countColumn;
 		}
-		$query->addSelect('course_id', DB::raw('('.implode(' + ', $totalCounts).') as totalCount'))
+	
+		$query->addSelect('course_meta_tags.course_id', DB::raw('('.implode(' + ', $totalCounts).') as totalCount'))
 			  ->where(DB::raw('('.implode(' + ', $totalCounts).')'), '>', 0)
 			  ->orderBy('totalCount', 'desc');
-
-		return $query->get()->pluck('course_id');
+	
+		return $query->get()->pluck('course_meta_tags.course_id');
 	}
+	
 
 	private static function splitStringIntoSegments($string, $segmentLength)
 	{
