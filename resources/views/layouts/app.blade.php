b848683cfbc08347f874e83e4103b3cfb8411b17
Alin <andreialin2324@yahoo.com>
2023-06-16 19:47:07 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta name="csrf-token" content="{{ csrf_token() }}" />
-		@if($isset($course))
+		@if(isset($course))
 		<meta name="description" content="{{ $course->descriprion }}" />
 		@else
 		<meta name="description" content="Inscriere gratuit la cursuri si descopera cunostinte noi">
