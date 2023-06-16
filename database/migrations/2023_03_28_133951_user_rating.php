1fb07d76c539b46a467b59dff7966634f430f926
Alin <andreialin2324@yahoo.com>
2023-06-16 17:30:07 +0300

    fix

---
+++ database/migrations/2023_03_28_133951_user_rating.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{ 
+		Schema::dropIfExists('user_ratings');
+	
+		Schema::create('user_activity', function (Blueprint $table) {
+			$table->id();
+			$table->unsignedBigInteger('user_id');
+			$table->string('activity_model');
+			$table->unsignedBigInteger('activity_id');
+			$table->decimal('rating', 3, 2)->default(0.00);
+			$table->text('review_text')->nullable();
+			$table->timestamps();
+	
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->index('user_id');
+		});
+	}
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		//
+	}
+};
---
+++ database/migrations/2023_05_06_210316_create_exercise_ordering_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up() : void
+	{ 
+		Schema::create('exercise_ordering_items', function (Blueprint $table) {
+			$table->id();
+			$table->unsignedBigInteger('exercise_id');
+			$table->foreign('exercise_id')->references('id')->on('exercises')->onDelete('cascade');
+			$table->text('item');
+			$table->integer('correct_order');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down() : void
+	{
+		Schema::dropIfExists('exercise_ordering_items');
+	}
+};
