3abaf6dcfdc3e1dd8938dd6d027ffc79422644b7
Alin1911 <andreialin2324@yahoo.com>
2024-04-28 23:35:09 +0300

    add controller

--- app/Http/Controllers/FlowersController.php
+++ app/Http/Controllers/FlowersController.php
@@ -1,8 +1,14 @@
+use Illuminate\Http\Request;
+
 <?php
 
 namespace App\Http\Controllers;
 
+
 class FlowersController extends Controller
 {
-	//
+	public function index()
+	{
+		return view('flowers.index');
+	}
 }
